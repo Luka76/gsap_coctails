5346c357379072fc75b0b7aa39b5c3fade0789b9
Luka76 <vargaluka76@gmail.com>
2025-07-08 20:46:24 +0200

    Navbar in progress

---
+++ public/constants/index.js
@@ -0,0 +1,8 @@
+export const navLinks = [
+    {
+        id: 'coctails', title: 'Cocktails'
+    },
+    {
+        id: 'about', title: 'About Us'
+    }
+]
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { navLinks } from "../../public/constants/index.js";
+
+const Navbar = () => {
+  return (
+    <nav>
+      <div>
+        <a href="#home" className="flex items-center gap-2">
+          <p>Velvet Pour</p>
+        </a>
+
+        <ul>
+          {navLinks.map((item) => (
+            <li key={item.id}>
+                <a href={`#${item.id}`}>{item.title}</a>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
